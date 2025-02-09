be04ce8bb8686c8f10e70e87afaddb74173c8977
akshay ashok <akshayashokan1054@gmail.com>
2025-02-09 19:26:17 +0530

    added battery monitoring

---
+++ app/src/main/java/com/akshayashokcode/androidbattery/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.akshayashokcode.androidbattery
+
+import androidx.activity.ComponentActivity
+import com.akshayashokcode.androidbattery.ui.BatteryViewModel
+
+class MainActivity: ComponentActivity() {
+    private val batteryViewModel = BatteryViewModel()
+
+}
